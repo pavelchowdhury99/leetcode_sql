53f3f13a550b0e62733b1e7af3bffe94a94f3e8b
pavelchowdhury99 <59216690+pavelchowdhury99@users.noreply.github.com>
2022-01-14 20:10:20 +0530

    Using DENSE_RANK()

--- Medium/Department Highest Salary.sql
+++ Medium/Department Highest Salary.sql
@@ -64,6 +64,36 @@ values
 (2,'Sales')
 ;
 
+CREATE table Employee
+(
+Id int, 
+Name varchar(50),
+salary int,
+DepartmentId int
+);
+
+CREATE table Department
+(
+id int,
+Name varchar(50)
+);
+
+
+INSERT into Employee 
+values
+(1,'Joe'  ,70000,1),
+(2,'Jim'  ,90000,1),
+(3,'Henry',80000,2),
+(4,'Sam'  ,60000,2),
+(5,'Max'  ,90000,1)
+;
+
+INSERT into Department 
+values
+(1,'IT'),
+(2,'Sales')
+;
+
 select Department,Employee,Salary from 
 (
 select d.Name as Department,
